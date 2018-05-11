fc9dad780d402dd437294a4c7e847ff87f5b6047
Gradleaders\ken.ward <kward@gradleaders.com>
2018-05-11 10:58:54 -0400

    First commit

---
+++ GameOfLife/GridManager.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class GridManager
+    {
+        // use a random number to generate the first grid
+        public static int[,] SetUpGrid(Random random)
+        {
+            var grid = new int[6, 8];
+            int rowLength = grid.GetLength(0);
+            int columnLength = grid.GetLength(1);
+            for (int row = 0; row < rowLength; row++)
+            {
+                for (int column = 0; column < columnLength; column++)
+                {
+                    if (random.Next(0, 9) < 5)
+                    {
+                        grid[row, column] = 0; // 0 = cell is dead
+                    }
+                    else
+                    {
+                        grid[row, column] = 1; // 1 = cell is alive
+                    }
+                }
+            }
+            return grid;
+        } // end SetUpGrid()
+
+        // show the grid with 6 rows and 8 columns
+        public static void OutputGrid(int[,] grid)
+        {
+            int rowLength = grid.GetLength(0);
+            int columnLength = grid.GetLength(1);
+            for (int row = 0; row < rowLength; row++)
+            {
+                for (int column = 0; column < columnLength; column++)
+                {
+                    Console.Write("{0} ", grid[row, column]);
+                }
+                Console.Write(Environment.NewLine);
+            }
+            Console.Write(Environment.NewLine);
+        } // end OutputGrid()
+
+        // use neighbor counts to create a new grid
+        public static int[,] UpdateGrid(int[,] grid)
+        {
+            int rowLength = grid.GetLength(0);
+            int columnLength = grid.GetLength(1);
+            var gridCopy = grid;
+            for (int row = 0; row < rowLength; row++)
+            {
+                for (int column = 0; column < columnLength; column++)
+                {
+                    if (gridCopy[row, column] < 2 || gridCopy[row, column] > 3)
+                    {
+                        grid[row, column] = 0;
+                    }
+                    else
+                    {
+                        grid[row, column] = 1;
+                    }
+                }
+            }
+            return grid;
+        } // end UpdateGrid()
+
+        // prepare the grid for helping count the neighbors
+        public static int[,] ChangeLives(int[,] grid)
+        {
+            int rowLength = grid.GetLength(0);
+            int columnLength = grid.GetLength(1);
+            var gridWithCounts = new int[6, 8];
+
+            for (int row = 0; row < rowLength; row++)
+            {
+                for (int column = 0; column < columnLength; column++)
+                {
+                    gridWithCounts[row, column] = CountLiveNeighbors(grid, rowLength, columnLength, row, column);
+                }
+            }
+            return gridWithCounts;
+        } // end ChangeLives()
+
+        // use the given coordinate to find how many of its neighbors are alive
+        private static int CountLiveNeighbors(int[,] grid, int rowLength, int columnLength, int row, int column)
+        {
+            int liveNeighborCount = 0;
+
+            for (int xPos = Math.Max(0, row - 1); xPos <= Math.Min(row + 1, rowLength - 1); xPos++)
+            {
+                for (int yPos = Math.Max(0, column - 1); yPos <= Math.Min(column + 1, columnLength - 1); yPos++)
+                {
+                    if ((xPos != row || yPos != column) && grid[xPos, yPos] == 1)
+                    {
+                        liveNeighborCount++;
+                    }
+                }
+            }
+            return liveNeighborCount;
+        } // end CountLiveNeighbors()
+    } // end GridManager
+} // end namespace
+
---
+++ GameOfLife/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int generation = 1;
+            int[,] gameGrid = GridManager.SetUpGrid(random);
+            Console.WriteLine("Welcome to Conway's Game of Life.");
+            System.Threading.Thread.Sleep(3000); // pause for 2 seconds
+            Console.WriteLine("0 = dead cell");
+            Console.WriteLine("1 = live cell");
+            System.Threading.Thread.Sleep(3000); // pause for 3 seconds
+            Console.Write(Environment.NewLine);
+            Console.WriteLine("Initial grid generated randomly:");
+            System.Threading.Thread.Sleep(3000); // pause for 3 seconds
+            GridManager.OutputGrid(gameGrid);
+            System.Threading.Thread.Sleep(3000); // pause for 3 seconds
+            Console.WriteLine("The game is beginning. Press the escape key to stop.");
+            System.Threading.Thread.Sleep(3000); // pause for 3 seconds
+            Console.Write(Environment.NewLine);
+            while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape))
+            {
+                int[,] gridCounter = GridManager.ChangeLives(gameGrid);
+                gameGrid = GridManager.UpdateGrid(gridCounter);
+                Console.WriteLine("Generation {0}", generation);
+                generation++;
+                GridManager.OutputGrid(gameGrid);
+                System.Threading.Thread.Sleep(1000); // pause for a second
+            }
+        } // end Main()
+    } // end Program
+} // end namespace
